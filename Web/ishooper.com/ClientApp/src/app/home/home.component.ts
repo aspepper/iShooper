1e24bec9a1a8da1943b69cdc7860e2b459b2beab
alexsandro.pimenta <aspepper@hotmail.com>
2020-07-07 10:16:46 -0300

    Atualizações

--- Web/ishooper.com/ClientApp/src/app/home/home.component.ts
+++ Web/ishooper.com/ClientApp/src/app/home/home.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css', './alefhebrew.css', './fonts-default.css','./lpCommon.css']
 })
 export class HomeComponent {
 }
